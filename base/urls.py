2e43d59faba9f2081b1ec295f71028d1789da91e
arash <gorjiarash21@gmail.com>
2023-12-09 18:24:04 +0330

    logout url is added

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, room, create_room, update_room, delete_room, login_page
+from .views import home, room, create_room, update_room, delete_room, login_page, logout_user
 
 urlpatterns = [
     path("login/", login_page, name="login"),
+    path("logout/", logout_user, name="logout"),
     path("", home, name="home"),
     path("room_page/<str:pk>", room, name="room"),
     path("create-room/", create_room, name="create-room"),
