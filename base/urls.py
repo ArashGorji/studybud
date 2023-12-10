94e8d139fa6d09b59a9b1f6af020a80191eb7077
arash <gorjiarash21@gmail.com>
2023-12-10 18:11:17 +0330

    add profile url

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home, room, create_room, update_room, delete_room, login_page, logout_user, register_user, \
-    delete_message
+    delete_message, user_profile
 
 urlpatterns = [
     path("login/", login_page, name="login"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("logout/", logout_user, name="logout"),
     path("", home, name="home"),
     path("room_page/<str:pk>", room, name="room"),
+    path("profile/<str:pk>", user_profile, name="user-profile"),
     path("create-room/", create_room, name="create-room"),
     path("update-room/<str:pk>", update_room, name="update-room"),
     path("delete-room/<str:pk>", delete_room, name="delete-room"),
