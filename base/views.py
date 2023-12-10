4fb15682f3cdb3eb2a1abe550ab1de634f242a81
arash <gorjiarash21@gmail.com>
2023-12-10 14:04:15 +0330

    add delete-message view

--- base/views.py
+++ base/views.py
@@ -78,16 +78,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by("-created")
-
+    participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get("body")
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants' : participants}
     return render(request, "base/room.html", context)
 
 
@@ -131,3 +132,16 @@ def delete_room(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {'obj': room})
+
+
+@login_required(login_url="/login")
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You are not allowed here!!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {'obj': message})
