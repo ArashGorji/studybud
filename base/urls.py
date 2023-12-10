7ac09267521e64e2b1cce2aea7a6460681542c59
arash <gorjiarash21@gmail.com>
2023-12-10 08:42:59 +0330

    register url is added.

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, room, create_room, update_room, delete_room, login_page, logout_user
+from .views import home, room, create_room, update_room, delete_room, login_page, logout_user,register_user
 
 urlpatterns = [
     path("login/", login_page, name="login"),
+    path("register/", register_user, name="register"),
     path("logout/", logout_user, name="logout"),
     path("", home, name="home"),
     path("room_page/<str:pk>", room, name="room"),
